afd2ccb35eb089ed2f7bde26209c899f8d70b3ce
Duc Hieu <tranthaiduchieu@gmail.com>
2023-10-29 22:14:05 +0700

    create StatsTable
    - StatsTable to show Order Overview

---
+++ src/Components/StatsTable.js
@@ -0,0 +1,16 @@
+export default function StatsTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Loại</th>
+          <th>Đơn hộp</th>
+          <th>Đơn lẻ</th>
+          <th>Khuyến mãi</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+}
